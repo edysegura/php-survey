e3562ae3b5a1eac1aaba4fb14c93438103182aa2
Edy Segura <edysegura@gmail.com>
2008-03-11 04:22:02 +0000

    Implementação do relatório geral

--- class/RelatorioGeralDAO.class.php
+++ class/RelatorioGeralDAO.class.php
@@ -6,12 +6,27 @@
  * 
  */
 
-class ParticipanteDAO extends DAO {
+class RelatorioGeralDAO extends DAO {
 	
-	public function getRelatorioGeral() {
+	public function getRelatorioGeral($itemPesquisaId, $avaliacaoId) {
+		$DB = $this->getDB();
+		
+		$sQuery = "SELECT count(avaliacao_id) AS total_votos FROM participante_avaliacao
+							 WHERE item_pesquisa_id = $itemPesquisaId and	avaliacao_id = $avaliacaoId
+							 GROUP BY avaliacao_id";
+		
+		$rsResult = $DB->executeSQL($sQuery);
+		
+		if($rsResult != NULL) {
+			$totalVotos = @mysql_result($rsResult, 0, "total_votos");
+			$totalVotos = ($totalVotos) ? $totalVotos : 0;
+		}
+		else {
+			$totalVotos = 0;
+		}
+		
+		return $totalVotos;
 	}
 	
-	public function getRelatorioGeralTO() {
-	}
-}
+};
 ?>
